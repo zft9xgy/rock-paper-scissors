0b433e9769ac9fb6d10d07e17f7e1e1dfbd4f1bd
zft9xgy <zft9xgy@icloud.com>
2023-07-04 21:00:49 +0200

    Created game() function and tested. Logic works. Pending to create guid interface

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 /*
-This function reutnr 'Rock', 'Paper' or 'Scissors' randomly.
+Introducción de parametros
+playerSelection -> deberá ser rock, paper, scissors
+computerSelection -> auto
 */
 
+// var playerSelection = prompt("Introduzca su eleccion:");
+// console.log("Antes de la validación:" + playerSelection);
+// playerSelection = validatePlayerSelection(playerSelection);
+// console.log("Después de la validación:" + playerSelection);
+
+// const computerSelection = getComputerChoice();
+// playRound(playerSelection, computerSelection);
+
+// function validatePlayerSelection(e) {
+//   return e.toLowerCase().trim();
+// }
+
+/*
+This function return 'Rock', 'Paper' or 'Scissors' randomly.
+*/
 function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 3); // return 0,1,2
+const choice = Math.floor(Math.random() * 3); // return 0,1,2
   switch (choice) {
     case 0:
       return "rock";
@@ -19,23 +36,58 @@ function getComputerChoice() {
   }
 }
 
-// Crear una funcion que juege una ronda
-// Esta funcion acepta dos parametros playerSelection, computerSelection
-// devuelve You win/lose + paper beat rock.
+/* 
+Crear una funcion que juege una ronda
+Esta funcion acepta dos parametros playerSelection, computerSelection
+devuelve You win/lose + paper beat rock.
+*/
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase().trim();
+  /* bool that us true is player win or false is lose*/
+  const playerwin =
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper");
 
-function singleRound(playerSelection, computerSelection) {
-playerSelection = playerSelection.toLowerCase();
-const playerwin = ((playerSelection == 'rock' && computerSelection =='scissors') || (playerSelection =='paper' && computerSelection =='rock') || (playerSelection == 'scissors' && computerSelection == 'paper'))
-
-   if (playerSelection == computerSelection) {
+  if (playerSelection == computerSelection) {
     // tie
-    return `Its a Tie both got ${playerSelection}`;
-   }else if (playerwin){
+    console.log(`Its a Tie both got ${playerSelection}`);
+    return `tie`;
+  } else if (playerwin) {
     // player player win
-    return `Yoy win ${playerSelection} win against ${computerSelection}`;
-   }else{
-    // player player lose 
-    return `You lose ${playerSelection} lost against ${computerSelection}`;
-   }
+    console.log(`Yoy win ${playerSelection} win against ${computerSelection}`);
+    return `win`;
+  } else {
+    // player player lose
+    console.log(`You lose ${playerSelection} lost against ${computerSelection}`);
+    return `lose`;
+  }
+}
 
+function game() {
+  // se juega hasta que alguno de los dos jugadores llegue a 5
+  var playerScore = 0;
+  var computerScore = 0;
+  var stopGame = false;
+  var score = 'tie'; 
+
+        do {
+            // pedir al jugador su opción
+            // play round
+            score = playRound(prompt("Introduzca su eleccion:"),getComputerChoice());
+            // actualizar score
+            switch (score) {
+                case 'win': ++playerScore; break;
+                case 'lose': ++computerScore; break;            
+                default: break;
+            }
+            stopGame = (playerScore == 5 || computerScore == 5);
+            // comprobar score por si hay un ganador
+                // stopgame = true
+            // seguir con siguiente
+            console.log('Player socre: ' + playerScore);
+            console.log('Computer score: ' + computerScore);
+            console.log('Stopgame variable: ' + stopGame);
+            
+        } while (!stopGame);
 }
