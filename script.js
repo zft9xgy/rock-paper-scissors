ef7b0c0a7ffc778bd075a8fd0d993321def9289c
zft9xgy <zft9xgy@icloud.com>
2023-07-04 19:44:56 +0200

    Created singleRound function

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /*
 This function reutnr 'Rock', 'Paper' or 'Scissors' randomly.
 */
@@ -7,16 +6,36 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3); // return 0,1,2
   switch (choice) {
     case 0:
-      return "Rock";
+      return "rock";
       break;
     case 1:
-      return "Paper";
+      return "paper";
       break;
     case 2:
-      return "Scissors";
+      return "scissors";
       break;
     default:
       break;
   }
 }
 
+// Crear una funcion que juege una ronda
+// Esta funcion acepta dos parametros playerSelection, computerSelection
+// devuelve You win/lose + paper beat rock.
+
+function singleRound(playerSelection, computerSelection) {
+playerSelection = playerSelection.toLowerCase();
+const playerwin = ((playerSelection == 'rock' && computerSelection =='scissors') || (playerSelection =='paper' && computerSelection =='rock') || (playerSelection == 'scissors' && computerSelection == 'paper'))
+
+   if (playerSelection == computerSelection) {
+    // tie
+    return `Its a Tie both got ${playerSelection}`;
+   }else if (playerwin){
+    // player player win
+    return `Yoy win ${playerSelection} win against ${computerSelection}`;
+   }else{
+    // player player lose 
+    return `You lose ${playerSelection} lost against ${computerSelection}`;
+   }
+
+}
