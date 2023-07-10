5ca260980fe5c223eac6573cad16770d8e09734a
zft9xgy <zft9xgy@icloud.com>
2023-07-10 19:47:24 +0200

    Finish logic of the game. Need visual changes

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/* Declares global variables */
+var playerScore = 0;
+var computerScore = 0;
+
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+
+const playerChoices = document.querySelectorAll(".btn-selection");
+playerChoices.forEach((choice) =>
+	choice.addEventListener("click", (e) => playRound(e.target.value))
+);
+
+function enableButtons() {
+	playerChoices.forEach((e) => (e.disabled = false));
+}
+
+function disableButtons() {
+	playerChoices.forEach((e) => (e.disabled = true));
+}
+
 /*
 This function return 'rock', 'paper' or 'scissors' randomly.
 */
@@ -17,11 +37,9 @@ function getComputerChoice() {
 
 /* 
 This a function that plays a round
-This function accepts two parameters playerSelection, computerSelection
-return 'tie','win','lose'
 */
 function playRound(playerSelection) {
-	// Clean player selection. Validation is not done in this version.
+	// Clean player selection.
 	playerSelection = playerSelection.toLowerCase().trim();
 	const computerSelection = getComputerChoice();
 
@@ -38,54 +56,61 @@ function playRound(playerSelection) {
 	} else if (playerwin) {
 		// player player win
 		console.log(`Yoy win ${playerSelection} win against ${computerSelection}`);
+		++playerScore;
+		updateScore();
+		checkGame();
 		return `win`;
 	} else {
 		// player player lose
 		console.log(
 			`You lose ${playerSelection} lost against ${computerSelection}`
 		);
+		++computerScore;
+		updateScore();
+		checkGame();
 		return `lose`;
 	}
 }
 
 /* This function is used at the end of the game() to declare a winner. */
-function declareWinner(winner, playerScore) {
-	winner = playerScore == 5 ? "Player" : "Computer";
+function declareWinner() {
+	const winner = playerScore == 5 ? "Player" : "Computer";
 	console.log(`***${winner} won this game.***`);
+	document.getElementById(
+		"winner"
+	).innerHTML = `***${winner} won this game.***`;
 }
 
 /*
 The plays end when player or computer score reach 5. 
 Declaration of variables inside the game function.
 */
-function game() {
-	var playerScore = 0;
-	var computerScore = 0;
-	var stopGame = false;
-	var score = "tie";
-	var winner = "";
+function startGame() {
+	playerScore = 0;
+	computerScore = 0;
+	updateScore();
+	enableButtons();
+	document.getElementById("winner").innerHTML = "";
+}
 
-	do {
-		// Ask user for choice and play a round in the same sentence.
-		score = playRound(prompt("Introduzca su eleccion:"), getComputerChoice());
-		// update score.
-		switch (score) {
-			case "win":
-				++playerScore;
-				break;
-			case "lose":
-				++computerScore;
-				break;
-			default:
-				break;
-		}
-		// game stop boolen when one player reach score 5.
-		stopGame = playerScore == 5 || computerScore == 5;
+/* Check if the there is a winner or not. */
 
-		// console results.
-		console.log("Player score: " + playerScore);
-		console.log("Computer score: " + computerScore);
-	} while (!stopGame);
+function endGame() {
+	declareWinner();
+	disableButtons();
+}
 
-	declareWinner(winner, playerScore);
+function isGameEnd() {
+	return playerScore == 5 || computerScore == 5;
+}
+
+function checkGame() {
+	if (isGameEnd()) {
+		endGame();
+	}
+}
+
+function updateScore() {
+	playerScoreDisplay.innerHTML = `<h3>Your score: ${playerScore}</h3>`;
+	computerScoreDisplay.innerHTML = `<h3>Comouter score: ${computerScore}</h3>`;
 }
